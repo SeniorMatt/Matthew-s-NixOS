d35a0fcd0350eb5116a19b0329da0ae2487e34d9
SeniorMatt <matthew.prakhov@gmail.com>
2025-12-19 21:08:57 +0500

    Ditched Firefox fully

--- nixos/hosts/t480/home.nix
+++ nixos/hosts/t480/home.nix
@@ -2,7 +2,6 @@
 {
   imports = [
     ../../modules/home-manager/shells/bash
-    ../../modules/home-manager/programs/firefox
     ../../modules/home-manager/programs/zen-browser
     ../../modules/home-manager/programs/git
     ../../modules/home-manager/programs/steam
